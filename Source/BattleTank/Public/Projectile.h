c4de9a42b21b7f90763d2efb0c4c187b1b6db77e
cas3man <30662233+cas3man@users.noreply.github.com>
2019-10-09 15:50:44 -0400

    Tank Firing Projectile, Forward Velocity

--- Source/BattleTank/Public/Projectile.h
+++ Source/BattleTank/Public/Projectile.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Projectile.generated.h"
@@ -15,12 +16,16 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
-protected:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	void LaunchProjectile(float Speed);
+
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
+	
 
 };
