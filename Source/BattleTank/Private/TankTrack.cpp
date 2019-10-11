f949a9a9e151842c99712da6b1a5b64c85d66ec4
cas3man <30662233+cas3man@users.noreply.github.com>
2019-10-11 13:25:17 -0400

    Micro-Commit Comments

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -9,4 +9,6 @@ void UTankTrack::SetThrottle(float Throttle)
 	//auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s: throttle: %f"), *Name, Throttle);
+
+	//TODO clamp actual value so player can't over-drive
 }
